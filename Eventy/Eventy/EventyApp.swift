bf2c81e0c662392062cd107d693d291d5892438b
Vraj Shah <vraj.s@simformsolutions.com>
2023-06-08 14:53:39 +0530

    Initial setup of xcode project

---
+++ Eventy/Eventy/EventyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventyApp.swift
+//  Eventy
+//
+//  Created by Vraj Shah on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct EventyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

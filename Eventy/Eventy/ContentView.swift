37c7ef7bfaa6f30c1b64d223efada35274198550
Vraj Shah <110172095+vraj-s-shah@users.noreply.github.com>
2023-06-08 14:55:01 +0530

    Initial setup of xcode project (#1)

---
+++ Eventy/Eventy/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Eventy
+//
+//  Created by Vraj Shah on 08/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
